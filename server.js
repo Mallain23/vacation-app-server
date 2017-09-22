4f1eb86abe85ef9d79d732294481c0f8d120d3fb
Mallain23 <mallain23>
2017-09-21 21:17:54 -0400

    path change

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(
     })
 );
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://designer-tapir-81850.netlify.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   next();
